bccfc9c7074be5f482719b6aeab012bb6f3e97ff
Joksan Flores <joksan.flores@itential.com>
2026-07-01 10:14:58 -0400

    Fix sleep-and-echo to parse --seconds CLI arg

--- sleep-and-echo.sh
+++ sleep-and-echo.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --seconds) seconds="$2"; shift 2 ;;
+    *) shift ;;
+  esac
+done
 sleep "$seconds" && echo "Done sleeping for $seconds second(s)."
